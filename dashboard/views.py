3d758098deb3d53cf0dd115aace84ee2f62f46f9
DanxzSleepy <wolfdangalaxy2@gmail.com>
2025-12-01 15:51:12 -0300

    feat: criando dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required, user_passes_test
 
-# Create your views here.
+def is_staff(user):
+    return user.is_staff  # Só admin/staff pode ver
+
+@login_required
+@user_passes_test(is_staff)
+def index(request):
+    return render(request, 'dashboard/index.html')
