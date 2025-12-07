9103d33cee883d7a7cf806be380081c4201acde6
DanxzSleepy <senacpi93@gmail.com>
2025-12-07 01:45:39 -0300

    Add debugging to startup script

--- start.sh
+++ start.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
 # Startup script for Railway deployment
 
-# Run Django migrations
+# Make sure we're in the right directory
+cd /app
+
+# Set Python path
+export PYTHONPATH=/app:$PYTHONPATH
+
+# Run Django migrations with error handling
 echo "Running migrations..."
 python manage.py migrate --noinput
+MIGRATE_STATUS=$?
+if [ $MIGRATE_STATUS -ne 0 ]; then
+    echo "Migration failed with exit code $MIGRATE_STATUS"
+    exit $MIGRATE_STATUS
+fi
 
 # Collect static files
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
+COLLECT_STATUS=$?
+if [ $COLLECT_STATUS -ne 0 ]; then
+    echo "Collect static failed with exit code $COLLECT_STATUS"
+    exit $COLLECT_STATUS
+fi
 
 # Start the server
 echo "Starting server..."
